d8ecb5d3a93e8842dccdd6d9843e8674d7c42b29
Rosen Krumov <rosen.krumov@kingsland.io>
2018-10-15 16:22:19 +0300

    replaced qtum with locktrip strings on certain places

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.qtum.Qtum-Qt";
+        return @"org.LockTrip.LockTrip-Qt";
     } else {
         return [self __bundleIdentifier];
     }
