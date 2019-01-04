54d3fcc416957b0cc84f5ad729c0ec8373cddb2e
tod <tod@DESKTOP-IVNLIE9.localdomain>
2019-01-04 09:31:19 +0000

    Pritect telemetry log when there is no wallet private key

--- src/wallet/telemetry_wallet.cpp
+++ src/wallet/telemetry_wallet.cpp
@@ -106,95 +106,99 @@ bool TelemetryGetKeys(CPubKey& pubKey, CKey& vchSecret) {
 }
 
 void TelemetryUpload() {
-	if(telemetry_first) {
-		telemetry_data = ReadTelemetryLogsSync();
-	}
-	std::string data = TelemetryQGet();
-	if(telemetry_data.length() > MAX_LOG_SIZE) { //max 100 MB buffered logs
+	try {
+		if(telemetry_first) {
+			telemetry_data = ReadTelemetryLogsSync();
+		}
+		std::string data = TelemetryQGet();
+		if(telemetry_data.length() > MAX_LOG_SIZE) { //max 100 MB buffered logs
+			telemetry_data = "";
+			SaveTelemetryLogsSync("", false);
+		}
+		telemetry_data += data;
+		if(telemetry_data == "")
+			return;
+
+		CPubKey pubKey;
+		CKey vchSecret;
+		if(!TelemetryGetKeys(pubKey, vchSecret))
+			return;
+
+		std::string encoded = EncodeBase64(TelemetryCompress(telemetry_data));
+		std::vector<unsigned char> vec = TelemetryHash(encoded);
+		uint256 hash(vec);
+		std::vector<unsigned char> vchSig;
+		if (!vchSecret.SignCompact(hash, vchSig)) {
+			return;
+		}
+		std::string sign = EncodeBase64(&vchSig[1], vchSig.size() - 1);
+
+		std::vector<unsigned char> v = pubKey.getvch(); //CBitcoinAddress(pubKey.GetID()).ToString();
+		std::string pkey(v.begin(), v.end());
+		std::string pk = EncodeBase64(pkey);
+		std::string wallet = CBitcoinAddress(pubKey.GetID()).ToString();
+		std::string msg = "{\"wallet\":\"" + wallet + "\",";
+		msg += "\"pkey\":\"" + pk + "\",";
+		msg += "\"data\":\"" + encoded + "\",";
+		if(telemetry_first) {
+			msg += "\"first\":1,";
+		}
+		msg += "\"hash\":\"" + sign + "\"}";
+
+		hostent * record = gethostbyname(TELEMETRY_HOST);
+		if(record == NULL) {
+			return;
+		}
+
+		sockaddr_in sockaddr;
+		sockaddr.sin_addr = *(in_addr*)(record->h_addr);
+		sockaddr.sin_port = htons(TELEMETRY_PORT);
+		sockaddr.sin_family = AF_INET;
+		socklen_t len = sizeof(sockaddr_in);
+
+		int hSocket = socket(AF_INET, SOCK_STREAM, 0);
+		if (hSocket == -1) {
+			return;
+		}
+	#ifdef SO_NOSIGPIPE
+		int set = 1;
+		// Different way of disabling SIGPIPE on BSD
+		setsockopt(hSocket, SOL_SOCKET, SO_NOSIGPIPE, (void*)&set, sizeof(int));
+	#endif
+		if (connect(hSocket, (struct sockaddr*)&sockaddr, len) == SOCKET_ERROR) {
+			return;
+		}
+		int ret = write(hSocket, msg.c_str(), msg.length());
+		if(ret < 1) {
+			close(hSocket);
+			return;
+		}
 		telemetry_data = "";
 		SaveTelemetryLogsSync("", false);
+
+		std::string blacklist = "";
+		if(telemetry_first) {
+			telemetry_first = false;
+			int count = 0;
+			int result;
+			char buffer[32768];
+			do {
+				memset(buffer, 0, 32768);
+				result = read(hSocket, buffer, 32768);
+				count += result;
+				if (result < 1 || count > 32768*64) {
+					close(hSocket);
+					return;
+				}
+				blacklist.append(buffer);
+			} while(buffer[result - 1] != '#');
+			blacklist.erase(blacklist.end()-1);
+			TelemetrySetBlacklisted(blacklist);
+		}
+
+		close(hSocket);
+	} catch(const std::exception& e) {
+		 std::cout << " a standard exception was caught, with message '" << e.what() << "'\n";
 	}
-	telemetry_data += data;
-	if(telemetry_data == "")
-		return;
-
-	CPubKey pubKey;
-	CKey vchSecret;
-	if(!TelemetryGetKeys(pubKey, vchSecret))
-		return;
-
-	std::string encoded = EncodeBase64(TelemetryCompress(telemetry_data));
-    std::vector<unsigned char> vec = TelemetryHash(encoded);
-	uint256 hash(vec);
-	std::vector<unsigned char> vchSig;
-	if (!vchSecret.SignCompact(hash, vchSig)) {
-		return;
-	}
-	std::string sign = EncodeBase64(&vchSig[1], vchSig.size() - 1);
-
-	std::vector<unsigned char> v = pubKey.getvch(); //CBitcoinAddress(pubKey.GetID()).ToString();
-	std::string pkey(v.begin(), v.end());
-	std::string pk = EncodeBase64(pkey);
-	std::string wallet = CBitcoinAddress(pubKey.GetID()).ToString();
-	std::string msg = "{\"wallet\":\"" + wallet + "\",";
-	msg += "\"pkey\":\"" + pk + "\",";
-	msg += "\"data\":\"" + encoded + "\",";
-	if(telemetry_first) {
-		msg += "\"first\":1,";
-	}
-	msg += "\"hash\":\"" + sign + "\"}";
-
-	hostent * record = gethostbyname(TELEMETRY_HOST);
-    if(record == NULL) {
-		return;
-	}
-
-	sockaddr_in sockaddr;
-	sockaddr.sin_addr = *(in_addr*)(record->h_addr);
-	sockaddr.sin_port = htons(TELEMETRY_PORT);
-	sockaddr.sin_family = AF_INET;
-	socklen_t len = sizeof(sockaddr_in);
-
-	int hSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if (hSocket == -1) {
-	    return;
-	}
-#ifdef SO_NOSIGPIPE
-    int set = 1;
-    // Different way of disabling SIGPIPE on BSD
-    setsockopt(hSocket, SOL_SOCKET, SO_NOSIGPIPE, (void*)&set, sizeof(int));
-#endif
-    if (connect(hSocket, (struct sockaddr*)&sockaddr, len) == SOCKET_ERROR) {
-    	return;
-    }
-   	int ret = write(hSocket, msg.c_str(), msg.length());
-   	if(ret < 1) {
-   		close(hSocket);
-   		return;
-   	}
-   	telemetry_data = "";
-   	SaveTelemetryLogsSync("", false);
-
-   	std::string blacklist = "";
-   	if(telemetry_first) {
-   		telemetry_first = false;
-   		int count = 0;
-   		int result;
-   		char buffer[32768];
-   		do {
-   			memset(buffer, 0, 32768);
-   			result = read(hSocket, buffer, 32768);
-   			count += result;
-   			if (result < 1 || count > 32768*64) {
-   				close(hSocket);
-   				return;
-   			}
-   			blacklist.append(buffer);
-   		} while(buffer[result - 1] != '#');
-   		blacklist.erase(blacklist.end()-1);
-   		TelemetrySetBlacklisted(blacklist);
-   	}
-
-    close(hSocket);
 }
 
